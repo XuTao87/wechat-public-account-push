69d6a42a997bba13e77b127c0199684750a99e58
XuTao87 <95163978+XuTao87@users.noreply.github.com>
2025-05-14 00:43:03 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -51,15 +51,15 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '2IhMZbgKwRjFF7HxmGNwGp31U_vAMh3Zdsp2D8TomT4',
+  // CALLBACK_TEMPLATE_ID: '2IhMZbgKwRjFF7HxmGNwGp31U_vAMh3Zdsp2D8TomT4',
 
-  CALLBACK_USERS: [
-    {
-      name: '自己',
-      // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oGLy77XHzHyuXV7uD4tpWt7l8WQA',
-    }
-  ],
+  // CALLBACK_USERS: [
+  //   {
+  //     name: '自己',
+  //     // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+  //     id: 'oGLy77XHzHyuXV7uD4tpWt7l8WQA',
+  //   }
+  // ],
 
 }
 
