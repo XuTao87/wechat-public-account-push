68bd6b108a1e1a56eb72b51360e76cf9634d80c6
XuTao87 <95163978+XuTao87@users.noreply.github.com>
2025-05-30 22:05:42 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -19,13 +19,13 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '萌萌大王',
+      name: '小豆豆',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oGLy77XHzHyuXV7uD4tpWt7l8WQA',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'pul5KXoAuBKcLmCSnhEcFkdpooFVKyuzCVhb3VElxLQ',
+      useTemplateId: 'xBeMiPyhwro-OCIkeFT-VfYD3MT1D8yZGKOTJB9WVD4',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '7-20',
+      horoscopeDate: '12-13',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
