188ac071ef08675ff3c66d8258ca7cc4927b0079
XuTao87 <95163978+XuTao87@users.noreply.github.com>
2025-05-30 22:08:55 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oGLy77XHzHyuXV7uD4tpWt7l8WQA',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'xBeMiPyhwro-OCIkeFT-VfYD3MT1D8yZGKOTJB9WVD4',
+      useTemplateId: 'dj1TSHFlMcs4aMWN0ZPRYqeyX2DeVaEEexRE_SV0GR0',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-13',
       festivals: [
